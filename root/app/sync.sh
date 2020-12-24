51d10563f7150a17d6b80d5af648c8cd0056e2e5
Jake Wharton <jw@squareup.com>
2020-12-24 10:35:52 -0500

    Import editorconfig and reformat script

--- root/app/sync.sh
+++ root/app/sync.sh
@@ -3,7 +3,7 @@
 echo "INFO: Starting sync.sh PID $$ $(date)"
 
 if [ -n "$HEALTHCHECK_ID" ]; then
-  curl -sS -X POST -o /dev/null "$HEALTHCHECK_HOST/$HEALTHCHECK_ID/start"
+	curl -sS -X POST -o /dev/null "$HEALTHCHECK_HOST/$HEALTHCHECK_ID/start"
 fi
 
 set -e
@@ -13,5 +13,5 @@ mbsync -V -a -c /config/mbsync.rc
 echo "INFO: Completed sync.sh PID $$ $(date)"
 
 if [ -n "$HEALTHCHECK_ID" ]; then
-  curl -sS -X POST -o /dev/null --fail "$HEALTHCHECK_HOST/$HEALTHCHECK_ID"
+	curl -sS -X POST -o /dev/null --fail "$HEALTHCHECK_HOST/$HEALTHCHECK_ID"
 fi
